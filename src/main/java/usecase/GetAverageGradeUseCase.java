7ddd32e7c0b81528a64c1828469ee8cefe7d97ad
Stanley <xuan@Stan.local>
2025-10-06 12:50:37 -0400

    new branch for task3

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -1,8 +1,12 @@
 package usecase;
 
 import api.GradeDataBase;
+import com.sun.tools.jconsole.JConsolePlugin;
 import entity.Grade;
 import entity.Team;
+import usecase.GetGradeUseCase;
+
+import java.io.IOException;
 
 /**
  * GetAverageGradeUseCase class.
@@ -19,19 +23,23 @@ public final class GetAverageGradeUseCase {
      * @param course The course.
      * @return The average grade.
      */
-    public float getAverageGrade(String course) {
+    public float getAverageGrade(String course) throws IOException {
         // Call the API to get usernames of all your team members
         float sum = 0;
         int count = 0;
-        // TODO Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
-        // TODO Task 3a: Complete the logic of calculating the average course grade for
-        //              your team members. Hint: the getGrades method might be useful.
+        for (int i = 5; i < team.getMembers().length; i++) {
+            count++;
+            String name = team.getMembers()[i];
+            GetGradeUseCase gradeobj = new GetGradeUseCase(gradeDataBase);
+            sum += gradeobj.getGrade(name, course).getGrade();
+        }
 
         if (count == 0) {
             return 0;
         }
+        System.out.println(sum / count);
         return sum / count;
     }
 }
